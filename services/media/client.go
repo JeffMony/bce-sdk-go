d3033a00a22dc0f1dccb2a83ea32797e91b46989
JeffMony <jeffmony@163.com>
2024-05-08 11:41:11 +0800

    fix : add test
    Signed-off-by: JeffMony <jeffmony@163.com>

--- services/media/client.go
+++ services/media/client.go
@@ -13,6 +13,40 @@ type Client struct {
 	*bce.BceClient
 }
 
+func NewMcpClientWithConfig(config *BosClientConfiguration) (*Client, error) {
+	var credentials *auth.BceCredentials
+	var err error
+	ak, sk, endpoint := config.Ak, config.Sk, config.Endpoint
+	if len(ak) == 0 && len(sk) == 0 { // to support public-read-write request
+		credentials, err = nil, nil
+	} else {
+		credentials, err = auth.NewBceCredentials(ak, sk)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if len(endpoint) == 0 {
+		endpoint = DEFAULT_SERVICE_DOMAIN
+	}
+	defaultSignOptions := &auth.SignOptions{
+		HeadersToSign: auth.DEFAULT_HEADERS_TO_SIGN,
+		ExpireSeconds: auth.DEFAULT_EXPIRE_SECONDS}
+	defaultConf := &bce.BceClientConfiguration{
+		Endpoint:                  endpoint,
+		Region:                    bce.DEFAULT_REGION,
+		UserAgent:                 bce.DEFAULT_USER_AGENT,
+		Credentials:               credentials,
+		SignOption:                defaultSignOptions,
+		Retry:                     bce.DEFAULT_RETRY_POLICY,
+		ConnectionTimeoutInMillis: bce.DEFAULT_CONNECTION_TIMEOUT_IN_MILLIS,
+		RedirectDisabled:          config.RedirectDisabled}
+	
+	v1Signer := &auth.BceV1Signer{}
+
+	client := &Client{bce.NewBceClient(defaultConf, v1Signer)}
+	return client, nil
+}
+
 // NewClient make MCP(media) service client with defualt configuration
 // endPoint value can chose bj and sz or gz defualt bj
 func NewClient(ak, sk, endPoint string) (*Client, error) {
